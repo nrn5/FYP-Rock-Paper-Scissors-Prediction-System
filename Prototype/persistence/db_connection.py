3f5156261d4abc934b837aaa959f06b5d35a430f
nrn5 <C22388271@mytudublin.ie>
2025-11-29 17:08:59 +0000

    refactor DB connection to make it more secure

--- Prototype/persistence/db_connection.py
+++ Prototype/persistence/db_connection.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2.pool import SimpleConnectionPool
 
 DB_NAME = "rps_project"
 DB_USER = "rps_user"
@@ -6,19 +7,16 @@ DB_PASSWORD = "rps_password"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
+_connection_pool = None
+
+def init_connection_pool():
+    pass
+
 def get_connection():
-    try:
-        conn = psycopg2.connect(
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            host=DB_HOST,
-            port=DB_PORT
-        )
-        print("\nSuccessfully connected to database")
-        return conn
-    except Exception as e:
-        print("\nFailed to connect to database:")
-        print(e)
-        return None
-    
+    pass
+
+def return_connection():
+    pass
+
+def close_pool():
+    pass
