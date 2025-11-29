39ae63e8ef6d2b3e1a2eceb60213c2fa947c3ef0
nrn5 <C22388271@mytudublin.ie>
2025-11-29 17:22:28 +0000

    refactor db connection

--- Prototype/persistence/db_connection.py
+++ Prototype/persistence/db_connection.py
@@ -9,14 +9,34 @@ DB_PORT = "5432"
 
 _connection_pool = None
 
-def init_connection_pool():
-    pass
+def init_connection_pool(minconn=1, maxconn=5):
+    """ init PostgreSQL connection pool """
+    global _connection_pool
+    if _connection_pool is None:
+        try:
+            _connection_pool = SimpleConnectionPool(minconn, maxconn, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
+            print("\n[db_connection.py] Connection pool initialised\n")
+        except Exception as e:
+            print("\n[db_connection.py] Could not create connection pool: ", e)
+            raise
 
 def get_connection():
-    pass
+    """ retrives connection from the pool """
+    if _connection_pool is None:
+        init_connection_pool()
+    try:
+        return _connection_pool.getconn()
+    except Exception as e:
+        print("\n[db_connection.py] Failed to get connection: ", e)
+        return None
 
-def return_connection():
-    pass
+def return_connection(conn):
+    """ returns a connection form the pool """
+    if _connection_pool and conn:
+        _connection_pool.putconn(conn)
 
 def close_pool():
-    pass
+    """ closes all connections in the pool """
+    if _connection_pool:
+        _connection_pool.closeall()
+        print("\n[db_connection.py] Conneciton pool closed")
