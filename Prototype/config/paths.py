80810804c5ed166b9558101bd518d2cf8658a82f
nrn5 <C22388271@mytudublin.ie>
2026-04-09 19:32:34 +0100

    added paths for rf model

--- Prototype/config/paths.py
+++ Prototype/config/paths.py
@@ -8,5 +8,10 @@ ROOT = Path(__file__).parent.parent
 # { MODELS }
 MODELS_DIR = ROOT/"business"/"models"
 GESTURE_RF_PATH = MODELS_DIR/"rps_model.pkl"
+LSTM_PATH = MODELS_DIR/"lstm_model.h5"
 HAND_LANDMARK_PATH = MODELS_DIR/"hand_landmarker.task"
 
+# { TRAINING DATA }
+TRAINING_DIR = ROOT/"training"
+DATA_DIR = TRAINING_DIR/"data"
+RF_DATA_PATH = DATA_DIR/"rf_training_data.csv"
