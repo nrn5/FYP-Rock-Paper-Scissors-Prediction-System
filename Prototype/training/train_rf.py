bae51454167383ba4969323393fb9bb56eae6f09
nrn5 <C22388271@mytudublin.ie>
2026-04-15 18:56:03 +0100

    format

--- Prototype/training/train_rf.py
+++ Prototype/training/train_rf.py
@@ -24,11 +24,7 @@ def build_model():
     )
 
 def train_model(x, y):
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y,
-        test_size=0.2,
-        random_state=42,
-        stratify=y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2,random_state=42,stratify=y)
     model = build_model()
     model.fit(x_train, y_train)
 
